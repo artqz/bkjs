2b4656dd6c819d04c060cee80ae256445a0ec4a6
artqz <djoctuk@yandex.ru>
2018-11-23 12:39:59 +0300

    update map.js

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,168 +1,70 @@
 import React, { Component } from 'react';
-import Konva from 'konva';
+import {Stage, Layer, Group, Image } from 'react-konva';
+//import Konva from 'konva';
 
 class Map extends Component {
-  drawMap() {    
-    const location = this.props.location; 
-    const rooms = location.rooms;
-    
-    const writeMessage = (message) => {
-      text.setText(message);
-      layer.draw();
-    }
-
-    var stage = new Konva.Stage({
-      container: 'container',
-      width: 500,
-      height: 268
-    });
-
-    var layer = new Konva.Layer();
-
-    var text = new Konva.Text({
-      x: 10,
-      y: 10,
-      fontFamily: 'Calibri',
-      fontSize: 24,
-      text: 'hello',
-      fill: 'black'
-    });
-    //Загрузка фона
-    console.log(location);
-    var imageBackground = new Image();
-    imageBackground.onload = function() {
-
-      layer.add(new Konva.Image({
-        x: 0,
-        y: 0,
-        image: imageBackground,
+    state = {
+        background: null,
+        exitButton: [],
+        images: [],
         width: 500,
         height: 268
-      })).on('click', () => {
-        console.log('pp');
-        ;
-      });
-      stage.add(layer);
     }
-    imageBackground.src = location.background;
-    writeMessage(imageBackground.src);
-    layer.add(text);
-    //
-    stage.add(layer);
-    const loadImages =(sources, callback) => {      
-      var images = {};     
+    componentDidMount() {
+        const location = this.props.location;
+        const rooms = location.rooms
 
-      for(var src in sources) {
-        images[src] = new Image();
-        images[src].onload = () => {
-          callback(images);
-        };   
-        images[src].src = sources[src];
-      }
-    }
+        //load background
+        const background = new window.Image();
+        background.src = location.background;
+        background.onload = () => this.setState({ background });
 
-    const buildStage = (images) => {
-      rooms.forEach((room, index) => {
-        let nameLayer = index;
-        layer.add(
-          new Konva.Image({
-            image: images[nameLayer],
-            x: room.x,
-            y: room.y
-          })
-          .on('mouseover', () => {
-            writeMessage('mouseover ' + room.name);
-          })
-          .on('mouseout', () => {
-            writeMessage('');
-          })
-          .on('click', () => {
-            this.props.changeLocation(room.id);
-          })
-          .cache()
-          .drawHitFromCache()
-        );  
-      });
-
-      layer.add(text);
-      stage.add(layer);
-    }
-
-    var sources = {};
-    
-    // for(var key in rooms) {
-    //   sources[key] = rooms[key].model;
-    // }
-
-    // loadImages(sources, buildStage);
-  }
-
-  drawMapTwo() {
-    const location = this.props.location;    
-    console.log(location);
-    
-    const mapCanvas = this.refs.map;
-    const mapContext = mapCanvas.getContext("2d");
-    const exitButton = {
-      'location_id': location.location_id,
-      'x': 1,
-      'y': 1,
-      'width': 50,
-      'height': 20
-    };
-    
-    const locationBackground = new Image(); 
-    locationBackground.src = location.background; 
-    locationBackground.onload = function() 
-    {     
-      mapContext.drawImage(this, 0, 0, 500, 268);   
-
-      location.rooms.forEach(room => {      
-        mapContext.fillRect(room.x,room.y,room.width,room.height);
-        mapContext.strokeRect(room.x,room.y,room.width,room.height);
-        mapContext.clearRect(room.x,room.y,room.width,room.height);
-      });
-
-      if(location.location_id !== null) {    
-        mapContext.fillRect(exitButton.x,exitButton.y,exitButton.width,exitButton.height);
-        mapContext.strokeRect(exitButton.x,exitButton.y,exitButton.width,exitButton.height);
-        mapContext.clearRect(exitButton.x,exitButton.y,exitButton.width,exitButton.height);
-      }
-    };
-
-    const MouseClick = (e) => {
-      const x = e.pageX - e.target.offsetLeft;
-      const y = e.pageY - e.target.offsetTop;
-      if(location.rooms.length) {
-        const rooms = location.rooms; 
-        const selectRoom = rooms.filter(room => x >= room.x && x <= room.x + room.width && y >= room.y && y <= room.y + room.height)[0];
-
-        if (selectRoom) this.props.changeLocation(selectRoom.id); 
-      }
-
-      if(location.location_id !== null) {
-        const selectExitButtom = (x >= exitButton.x && x <= exitButton.x + exitButton.width && y >= exitButton.y && y <= exitButton.y + exitButton.height) ? true : false;
+        //load rooms
+        const images = rooms.map((room, index) => {
+            let image = new window.Image();
+            
+            image.onload = () => {
+                this.setState({ images }); 
+                this.group.children[index].cache().drawHitFromCache();     
+            }               
+            image.src = room.model;
+            return {image: image, x: room.x, y: room.y, id: room.id}
+        })
         
-        if (selectExitButtom) this.props.changeLocation(exitButton.location_id);      
-      }
+        //load exit
+        const exitImage = new window.Image();
+        exitImage.src = '/assets/exit.png'
+        exitImage.onload = () => this.setState({ exitButton: { image: exitImage, location_id: location.location_id }}); 
     }
 
-    mapCanvas.addEventListener('click', MouseClick);
-  }
-
-  componentDidMount() {
-    this.drawMap();      
-  }
-
-  render() {    
-    return (
-        <div>
-          <div id="container"></div>
-          <canvas ref="map" id="myCanvas" width="500" height="268"></canvas>
-          {this.props.location.name}         
-        </div>
-    );
-  }
+    handleClick (id) {
+        this.props.changeLocation(id);
+    }
+    
+    render() {   
+        const images = this.state.images.map((image, index) => (
+            <Image 
+                key={image.id} 
+                image={image.image} 
+                x={image.x}
+                y={image.y}
+                cahe
+                drawHitFromCache
+                onClick={() => this.handleClick(image.id)}
+            />
+        ));       console.log(this.state);
+        
+         
+        return (
+            <Stage width={this.state.width} height={this.state.height}>
+                <Layer>
+                    <Image image={this.state.background} />
+                    <Image image={this.state.exitButton.image} onClick={() => this.handleClick(this.state.exitButton.location_id)} />
+                    <Group ref={node => this.group = node}>{images}</Group>{/* <Image drawHitFromCache image={this.state.image} onClick={this.handleClick} ref={shape => this.imageShape = shape} /> */}
+                </Layer>
+            </Stage>
+        );
+    }
 }
-
+    
 export default Map;
---
+++ src/components/MapOld.js
@@ -0,0 +1,168 @@
+import React, { Component } from 'react';
+import Konva from 'konva';
+
+class Map extends Component {
+  drawMap() {    
+    const location = this.props.location; 
+    const rooms = location.rooms;
+    
+    const writeMessage = (message) => {
+      text.setText(message);
+      layer.draw();
+    }
+
+    var stage = new Konva.Stage({
+      container: 'container',
+      width: 500,
+      height: 268
+    });
+
+    var layer = new Konva.Layer();
+
+    var text = new Konva.Text({
+      x: 10,
+      y: 10,
+      fontFamily: 'Calibri',
+      fontSize: 24,
+      text: 'hello',
+      fill: 'black'
+    });
+    //Загрузка фона
+    console.log(location);
+    var imageBackground = new Image();
+    imageBackground.onload = function() {
+
+      layer.add(new Konva.Image({
+        x: 0,
+        y: 0,
+        image: imageBackground,
+        width: 500,
+        height: 268
+      })).on('click', () => {
+        console.log('pp');
+        ;
+      });
+      stage.add(layer);
+    }
+    imageBackground.src = location.background;
+    writeMessage(imageBackground.src);
+    layer.add(text);
+    //
+    stage.add(layer);
+    const loadImages =(sources, callback) => {      
+      var images = {};     
+
+      for(var src in sources) {
+        images[src] = new Image();
+        images[src].onload = () => {
+          callback(images);
+        };   
+        images[src].src = sources[src];
+      }
+    }
+
+    const buildStage = (images) => {
+      rooms.forEach((room, index) => {
+        let nameLayer = index;
+        layer.add(
+          new Konva.Image({
+            image: images[nameLayer],
+            x: room.x,
+            y: room.y
+          })
+          .on('mouseover', () => {
+            writeMessage('mouseover ' + room.name);
+          })
+          .on('mouseout', () => {
+            writeMessage('');
+          })
+          .on('click', () => {
+            this.props.changeLocation(room.id);
+          })
+          .cache()
+          .drawHitFromCache()
+        );  
+      });
+
+      layer.add(text);
+      stage.add(layer);
+    }
+
+    var sources = {};
+    
+    // for(var key in rooms) {
+    //   sources[key] = rooms[key].model;
+    // }
+
+    // loadImages(sources, buildStage);
+  }
+
+  drawMapTwo() {
+    const location = this.props.location;    
+    console.log(location);
+    
+    const mapCanvas = this.refs.map;
+    const mapContext = mapCanvas.getContext("2d");
+    const exitButton = {
+      'location_id': location.location_id,
+      'x': 1,
+      'y': 1,
+      'width': 50,
+      'height': 20
+    };
+    
+    const locationBackground = new Image(); 
+    locationBackground.src = location.background; 
+    locationBackground.onload = function() 
+    {     
+      mapContext.drawImage(this, 0, 0, 500, 268);   
+
+      location.rooms.forEach(room => {      
+        mapContext.fillRect(room.x,room.y,room.width,room.height);
+        mapContext.strokeRect(room.x,room.y,room.width,room.height);
+        mapContext.clearRect(room.x,room.y,room.width,room.height);
+      });
+
+      if(location.location_id !== null) {    
+        mapContext.fillRect(exitButton.x,exitButton.y,exitButton.width,exitButton.height);
+        mapContext.strokeRect(exitButton.x,exitButton.y,exitButton.width,exitButton.height);
+        mapContext.clearRect(exitButton.x,exitButton.y,exitButton.width,exitButton.height);
+      }
+    };
+
+    const MouseClick = (e) => {
+      const x = e.pageX - e.target.offsetLeft;
+      const y = e.pageY - e.target.offsetTop;
+      if(location.rooms.length) {
+        const rooms = location.rooms; 
+        const selectRoom = rooms.filter(room => x >= room.x && x <= room.x + room.width && y >= room.y && y <= room.y + room.height)[0];
+
+        if (selectRoom) this.props.changeLocation(selectRoom.id); 
+      }
+
+      if(location.location_id !== null) {
+        const selectExitButtom = (x >= exitButton.x && x <= exitButton.x + exitButton.width && y >= exitButton.y && y <= exitButton.y + exitButton.height) ? true : false;
+        
+        if (selectExitButtom) this.props.changeLocation(exitButton.location_id);      
+      }
+    }
+
+    mapCanvas.addEventListener('click', MouseClick);
+  }
+
+  componentDidMount() {
+    this.drawMap();      
+  }
+
+  render() {    
+    return (
+        <div>
+          <div id="container"></div>
+          <canvas ref="map" id="myCanvas" width="500" height="268"></canvas>
+          {this.props.location.name}         
+        </div>
+    );
+  }
+}
+
+export default Map;
