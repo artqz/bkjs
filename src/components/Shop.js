00653fcf1fe237299dbb05649482b3a410a2542f
artqz <djoctuk@yandex.ru>
2018-11-27 20:16:53 +0500

    update shop window

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+class Item extends Component {
+    render() {
+        const { item } = this.props;
+        const pathItems = '/assets'
+        return (
+            <div><img src={pathItems + item.icon} /> {item.name} - {item.price}</div>
+        );
+    }
+}
+
 class Shop extends Component {
     state = {
+        shopName: null,
         items: []
     }
     getShopInfo() {
-        axios.get(`http://127.0.0.1:8000/api/shops/${this.props.location}`).then(res => {
-            this.setState({ items: res.data.items });
+        axios.get(`http://127.0.0.1:8000/api/shops/location/${this.props.location}`).then(res => {
+            this.setState({ items: res.data.items, shopName: res.data.name });
         })
       }
     componentDidMount() {
        this.getShopInfo();
     }
     render() {
-        const { items } = this.state;
+        const { items, shopName } = this.state;
         
         return (
-            <div>
-                {items.map(item => (<div key={item.id}>{item.name}</div>))}
-              Shop             
+            <div>       
+                {shopName}         
+                {items.map(item => <Item key={item.id} item={item} />)}                           
             </div>
         );
     }
