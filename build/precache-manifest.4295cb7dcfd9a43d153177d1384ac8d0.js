0502a277a9afaae449cdac87e03a5db021ddde68
artqz <djoctuk@yandex.ru>
2018-12-27 13:41:31 +0500

    build

---
+++ build/precache-manifest.4295cb7dcfd9a43d153177d1384ac8d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f36caf546d2ebbace7a4",
+    "url": "/static/js/main.f36caf54.chunk.js"
+  },
+  {
+    "revision": "9d68cfdca0534a1dfa02",
+    "url": "/static/js/1.9d68cfdc.chunk.js"
+  },
+  {
+    "revision": "f36caf546d2ebbace7a4",
+    "url": "/static/css/main.318bb177.chunk.css"
+  },
+  {
+    "revision": "5089171caaf441e6d11d79f73c796edb",
+    "url": "/index.html"
+  }
+];
